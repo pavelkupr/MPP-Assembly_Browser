9b515b5ed1bb250d494bbcfa8a58170311fd4abd
Pavel <pavelkupr88@gmail.com>
2018-10-25 22:34:02 +0300

    Add unit tests

---
+++ AssemblyBrowser/AssemblyBrowserTests/AssemblyInfoGeneratorTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AssemblyBrowser;
+
+namespace AssemblyBrowserTests
+{
+	[TestClass]
+	public class AssemblyInfoGeneratorTests
+	{
+		private static AssemblyInfo assemblyInfo;
+
+		[ClassInitialize]
+		public static void ClassInitilize(TestContext context)
+		{
+			AssemblyInfoGenerator generator = new AssemblyInfoGenerator();
+			assemblyInfo = generator.GenerateAssemblyInfo("AssemblyBrowserTests.dll");
+		}
+
+		[TestMethod]
+		public void AssemblyInfoGenerator_Generate_AssemblyInfo()
+		{
+			Assert.IsNotNull(assemblyInfo);
+		}
+		[TestMethod]
+		public void AssemblyInfo_Has_correct_Name()
+		{
+			if(assemblyInfo.Name != "AssemblyBrowserTests")
+				Assert.Fail("Name of assembly is incorrect");
+		}
+		[TestMethod]
+		public void AssemblyInfo_Has_Right_Namespaces()
+		{
+			if( assemblyInfo.Namespaces.ToArray()[0].Name != "AssemblyBrowserTests")
+				Assert.Fail("Name of namespace is incorrect");
+		}
+
+		[TestMethod]
+		public void Namespace_Has_Right_Type()
+		{
+			NamespaceInfo namespaceInfo = assemblyInfo.Namespaces.ToArray()[0];
+
+			if (namespaceInfo.TypesInfo.ToArray()[0].Name != "AssemblyInfoGeneratorTests")
+				Assert.Fail("Name of type is incorrect");
+		}
+	}
+}
---
+++ AssemblyBrowser/AssemblyBrowserTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AssemblyBrowserTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AssemblyBrowserTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("656da0d8-09f1-4ec6-9612-d9e386881a92")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
